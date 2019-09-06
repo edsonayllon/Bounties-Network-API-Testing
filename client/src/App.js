c07cca62aeff110735c990d17115119d4eed436c
eayllon1 <eayllon1@gmail.com>
2019-09-06 07:39:44 -0400

    query bounties api and ipfs endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -4,28 +4,42 @@ import './App.css';
 
 function App() {
   const getApi = async () => {
-    console.log(`hello`)
+    // this example shows all active bounties 
+    // for the gitcoin platform
+    // sorted by reward in USD
+    // for only one platform, here gitcoin
+    // only showing open bounties
+    let res = await fetch(`https://api.bounties.network/bounty/?ordering=usd_price&platform=gitcoin&bountyStage=1`)
+    let json = await res.json();
+    return json
+  }
+
+  const createCard = async () => {
+    const bounties = await getApi();
+    console.log(bounties)
+  }
+
+  const getIpfsData = async () => {
+    // gets bounty details from ipfs gateway
+    let res = await fetch(`https://ipfs.io/ipfs/QmPXcbkWrKyxAcSrg5TXUM1QjuFtwSYTkXERNLoWrRQMno`)
+    let text = await res.json()
+    console.log(text);
   }
 
   useEffect(() => {
-    getApi()
+    createCard();
+    getIpfsData();
   },[])
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h2>Bounty Title</h2>
+          <p>
+            Bounty description
+          </p>
+        </div>
       </header>
     </div>
   );
